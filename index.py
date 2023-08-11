ea9ca5182754d714ba9d5a6708621ba392ae66df
Eurphus <petermacdonald2005@gmail.com>
2023-08-11 11:37:12 -0400

    Example of index file

--- index.py
+++ index.py
@@ -1,8 +1,24 @@
-from functions import encode_docs
+import datetime
+import logging
+import os
 
+from Loader import MacLoader
+from databases.PineconeDB import PineconeDB
 
-encode_docs(
-    "JSON",
-    batch_size=12,
-    max_num=24
+logging_folder = os.getcwd() + "/logs/"
+date_F = datetime.datetime.now().strftime("%d.%b_%Y_%H.%M.%S")
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=logging_folder + f"{date_F}.txt"
+)
+database = PineconeDB(
+    ensure_exists=True
+)
+
+loader = MacLoader()
+loader.encode_docs(
+    database=database,
+    max_num_files=100,
+    batch_size=5
 )
